c41a1c95e5eee0dbc5dc55ea485cda8c82aece51
zisyang <zishun.yang@sjsu.edu>
2022-04-19 03:01:22 -0700

    commit to backup aws demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wild Rydes
+
+The project is a web application that enables users to request unicorn rides. It was developed following [this AWS tutorial](https://aws.amazon.com/getting-started/hands-on/build-serverless-web-app-lambda-apigateway-s3-dynamodb-cognito/). The application architecture uses AWS Lambda, Amazon API Gateway, Amazon DynamoDB, Amazon Cognito, and AWS Amplify Console. 
---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'sa-east-1_wzkpdlOcg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4j3hf9ouu2thgs0jlk7h35tdbl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'sa-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://an2eirbqsk.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
